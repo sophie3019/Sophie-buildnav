5f3f448dbb5b995756816d752de42c2ff5b78c5e
Sophie <sophie.sa43@gmail.com>
2022-07-14 09:11:23 +0200

    added product

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const tootbar = props => {
+    <header>
+        <nav>
+            <div></div>
+                <div><a href='/'>The LOGO</a></div>
+                    <div>
+                        <ul>
+                            <li><a href='/'>Products</a></li>
+                            <li><a href='/'>Users</a></li>
+                        </ul>
+                
+            </div>
+        </nav>
+    </header>
+};
+
+export default tootbar;
